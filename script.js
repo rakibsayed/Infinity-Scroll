ee0ee6ff6f2b27d23452d5efe8cfa2610be23a06
rakibsayed <rakibsayed@outlook.in>
2022-08-26 13:28:20 +0530

    Fixed the Bug
    fixed the dumb little mistake appending the created element as a string rather than thier assign variable .... and created a separated set attributes so that i dont have to repeat myself

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const imageContainer = document.getElementById('img-container');
 const loader = document.getElementById('loader');
-
+console.log(imageContainer)
 let photosArray = [];
 
 // Unsplash API
@@ -9,23 +9,31 @@ const accesskey = "01gtue33kGsLSLeTHI8TmQNim0l6vkQXrpZAYTfRwEM";
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${accesskey}&count=${count}`;
 
 
+// Helper Function to Set Attributes on DOM Eelments
+function setAttributes(element , attributes){
+    for (const key in attributes){
+        element.setAttribute(key , attributes[key]);
+    }
+}
 // Create Elemnets for Links and Photos
 function displayPhotos() {
     photosArray.forEach(photo =>{
+        console.log(photosArray)
         // Create anchor element ti link Unsplash
         const item = document.createElement('a');
-        item.setAttribute('href' , photo.links.html);
-        item.setAttribute('target' , '_blank');
+        setAttributes(item , {href: photo.links.html , target: '_blank'});
 
         // Create <img> for Photo
         const img = document.createElement('img');
-        img.setAttribute('src' , photo.urls.regular);
-        img.setAttribute('alt' , photo.alt_description);
-        img.setAttribute('title' , photo.alt_description);
-
-        // Put <img> inside <a> , the put inside in the img Container
-        item.appendChild('img');
-        imageContainer.appendChild('item');
+        setAttributes(img , {
+            src: photo.urls.regular ,
+            alt: photo.alt_description,
+            title: photo.alt_description 
+        })
+    
+        // Put <img> inside <a> , the put <a> inside in the img Container
+        item.appendChild(img);
+        imageContainer.appendChild(item);
     });
 }
 
